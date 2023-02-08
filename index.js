f55737b67030538bc88f1bd6143657254b71713b
ceotyz <121935597+ceotyz@users.noreply.github.com>
2023-02-08 22:15:00 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+class Sheep {
+ constructor(name, age); {
+   this.name = name;
+   this.age = age;
+   function isItEtable(true/false);
+
+class Cow {
+ constructor(name, age); {
+   this.name = name;
+   this.age = age;
+   function isItEtable(true/false);
+ }
+class Rabbit {
+ constructor(name, age); {
+   this.name = name;
+   this.age = age;
+   function isItEtable(true/false);
+
+let Sheep = ["Ann" , "Sasha"];
+
+
+
+
+
+
+
+
+
+
+
+
+
