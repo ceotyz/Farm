3b8848f1dd163e13fad3fb1554fed73782d70c4d
ceotyz <121935597+ceotyz@users.noreply.github.com>
2023-02-11 20:16:54 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
-class Sheep {
- constructor(name, age); {
+class Animal {
+ constructor(name, age) {
    this.name = name;
    this.age = age;
-   function isItEtable(true/false);
 
-class Cow {
- constructor(name, age); {
-   this.name = name;
-   this.age = age;
-   function isItEtable(true/false);
+ if (animals.length > 0) {
+	 output += animals.reduce((acc, current) => {
+		  let count = 0;
+		  return acc + current.age + count;
+   }, 0);
  }
-class Rabbit {
- constructor(name, age); {
-   this.name = name;
-   this.age = age;
-   function isItEtable(true/false);
+ function generateAnimals(type, count) {
+ let output = [];
 
-let Sheep = ["Ann" , "Sasha"];
+ for (let i = 0; i <= count; i++) {
+   let parameter = {
+     name: `${type} ${i}`,
+     age: i,
+   };
+
+   let item = null;
+
+   switch (type) {
+     case "Rabbit":
+       item = new Rabbit(parameter.name, parameter.age);
+       break;
+     case "Sheep":
+       item = new Sheep(parameter.name, parameter.age);
+       break;
+     case "Cow":
+       item = new Cow(parameter.name, parameter.age);
+       break;
+   }
+    if (item) {
+     output.push(item);
+   }
+ }
+
+ return output;
+}
+console.log(getAnimalsAge(animals))
 
 
 
